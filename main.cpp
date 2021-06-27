666e081774f88fcf76ba561481f0778f4390a1f4
FB <fbaeuerlein@gmail.com>
2021-06-27 21:47:16 +0200

    Restructured submodules and benchmarks

--- main.cpp
+++ main.cpp
@@ -27,18 +27,18 @@ void error_handler()
     (void)some_task();
 }
 
-bool error_injector()
+bool errorInjector(int num)
 {
-    return std::rand() % 10 == 0;
+    return std::rand() % num == 0;
 }
 
-int return_value_by_reference(double &value)
+int return_value_by_reference(double &value, std::function<bool()> const & error_injector)
 {
     value = some_task();
     return error_injector() ? E_FAIL : E_OK;
 }
 
-double return_value_with_exception()
+double return_value_with_exception(std::function<bool()> const & error_injector)
 {
     double value = some_task();
     if (error_injector())
@@ -46,7 +46,7 @@ double return_value_with_exception()
     return value;
 }
 
-std::optional<double> return_value_with_optional()
+std::optional<double> return_value_with_optional(std::function<bool()> const & error_injector)
 {
     double value = some_task();
     if (error_injector())
@@ -54,7 +54,7 @@ std::optional<double> return_value_with_optional()
     return value;
 }
 
-tl::expected<double, int> return_value_with_expected()
+tl::expected<double, int> return_value_with_expected(std::function<bool()> const & error_injector)
 {
     double value = some_task();
     if (error_injector())
@@ -62,38 +62,42 @@ tl::expected<double, int> return_value_with_expected()
     return value;
 }
 
-void do_expected()
+void do_expected(int x)
 {
-    auto val = return_value_with_expected();
+    auto error_injector = [=](){ return errorInjector(x); };
+    auto val = return_value_with_expected(error_injector);
     if (!val)
     {
         error_handler();
     }
 }
 
-void do_by_ref()
+void do_by_ref(int x)
 {
     double value = 0;
-    if (return_value_by_reference(value) != E_OK)
+    auto error_injector = [=](){ return errorInjector(x); };
+    if (return_value_by_reference(value, error_injector) != E_OK)
     {
         error_handler();
     }
 }
 
-void do_with_optional()
+void do_with_optional(int x)
 {
-    std::optional<double> value = return_value_with_optional();
+    auto error_injector = [=](){ return errorInjector(x); };
+    std::optional<double> value = return_value_with_optional(error_injector);
     if (!value)
     {
         error_handler();
     }
 }
 
-void do_with_exception()
+void do_with_exception(int x)
 {
+    auto error_injector = [=](){ return errorInjector(x); };
     try
     {
-        double value = return_value_with_exception();
+        double value = return_value_with_exception(error_injector);
     }
     catch (const std::exception &e)
     {
@@ -105,7 +109,7 @@ static void BM_do_by_ref(benchmark::State &state)
 {
     for (auto _ : state)
     {
-        do_by_ref();
+        do_by_ref(state.range(0));
     }
 }
 
@@ -113,7 +117,7 @@ static void BM_do_expected(benchmark::State &state)
 {
     for (auto _ : state)
     {
-        do_expected();
+        do_expected(state.range(0));
     }
 }
 
@@ -121,7 +125,7 @@ static void BM_do_with_optional(benchmark::State &state)
 {
     for (auto _ : state)
     {
-        do_with_optional();
+        do_with_optional(state.range(0));
     }
 }
 
@@ -129,13 +133,33 @@ static void BM_do_with_exception(benchmark::State &state)
 {
     for (auto _ : state)
     {
-        do_with_exception();
+        do_with_exception(state.range(0));
     }
 }
 
-BENCHMARK(BM_do_with_exception);
-BENCHMARK(BM_do_with_optional);
-BENCHMARK(BM_do_expected);
-BENCHMARK(BM_do_by_ref);
+BENCHMARK(BM_do_with_exception)->Arg(500);
+BENCHMARK(BM_do_with_exception)->Arg(100);
+BENCHMARK(BM_do_with_exception)->Arg(10);
+BENCHMARK(BM_do_with_exception)->Arg(5);
+BENCHMARK(BM_do_with_exception)->Arg(2);
+
+
+BENCHMARK(BM_do_with_optional)->Arg(500);
+BENCHMARK(BM_do_with_optional)->Arg(100);
+BENCHMARK(BM_do_with_optional)->Arg(10);
+BENCHMARK(BM_do_with_optional)->Arg(5);
+BENCHMARK(BM_do_with_optional)->Arg(2);
+
+BENCHMARK(BM_do_expected)->Arg(500);
+BENCHMARK(BM_do_expected)->Arg(100);
+BENCHMARK(BM_do_expected)->Arg(10);
+BENCHMARK(BM_do_expected)->Arg(5);
+BENCHMARK(BM_do_expected)->Arg(2);
+
+BENCHMARK(BM_do_by_ref)->Arg(500);
+BENCHMARK(BM_do_by_ref)->Arg(100);
+BENCHMARK(BM_do_by_ref)->Arg(10);
+BENCHMARK(BM_do_by_ref)->Arg(5);
+BENCHMARK(BM_do_by_ref)->Arg(2);
 
 BENCHMARK_MAIN();
